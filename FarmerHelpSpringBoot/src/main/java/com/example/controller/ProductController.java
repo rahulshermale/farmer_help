c7d93e4bcc45476c22267126a652bc0b1f31ad60
Rahul <rahulshermale50@gmail.com>
2024-03-17 08:33:28 +0530

    i am worked on Product component

--- FarmerHelpSpringBoot/src/main/java/com/example/controller/ProductController.java
+++ FarmerHelpSpringBoot/src/main/java/com/example/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.example.entity.Products;
 import com.example.services.ProductServicesimpl;
 
 @RestController
+@CrossOrigin("*")
 public class ProductController {
 
 	@Autowired
